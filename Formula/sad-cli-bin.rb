0b672a64e1266f44ead2432437ad4f608c7a2633
Krzysztof Rodak <krodak.konta@gmail.com>
2026-02-26 23:40:39 +0100

    brew: update formulae with release SHA256 hashes

--- Formula/sad-cli-bin.rb
+++ Formula/sad-cli-bin.rb
@@ -3,7 +3,7 @@ class SadCliBin < Formula
   homepage "https://github.com/krodak/sad-cli"
   url "https://github.com/krodak/sad-cli/releases/download/v#{version}/sad-macos-universal.tar.gz"
   version "0.1.0"
-  sha256 "PLACEHOLDER"
+  sha256 "b4a70e4feae1f847dd7890aef04dddb62c6cc63858fdf6b39ee671564eb885a7"
   license "MIT"
 
   depends_on :macos
--- Formula/sad-cli.rb
+++ Formula/sad-cli.rb
@@ -3,7 +3,7 @@ class SadCli < Formula
   homepage "https://github.com/krodak/sad-cli"
   url "https://github.com/krodak/sad-cli/archive/refs/tags/v#{version}.tar.gz"
   version "0.1.0"
-  sha256 "PLACEHOLDER"
+  sha256 "53f7e2861adee02a43ef0d76bfeb0d7908bb4524d5179165b4b269e2b33726a9"
   license "MIT"
 
   depends_on xcode: ["16.0", :build]
